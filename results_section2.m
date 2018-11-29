e1bc2d5f6e47c78d6cadac7d0b9d1f364aaae87e
Ben Griffiths <benjaminGriffiths@users.noreply.github.com>
2018-11-29 11:02:40 +0000

    partially updated EEG script to work with BIDS directories

--- results_section2.m
+++ results_section2.m
@@ -5,121 +5,41 @@ close all
 clc
 
 % define root directory
-if ispc;    dir_root = 'Y:/projects/reinstatement_fidelity/';
-            dir_tool = 'Y:/projects/general';
-            dir_repos  = 'E:/bjg335/projects/reinstatement_fidelity/'; % repository directory
-else;       dir_root = '/media/bjg335/rds-share-2018-hanslmas-memory/projects/reinstatement_fidelity';
+if ispc;    dir_bids = 'Y:/projects/reinstatement_fidelity/bids_data/';
+            dir_tool = 'Y:/projects/general/';
+            dir_repos  = 'E:/bjg335/projects/reinstatement_fidelity/bids_data/'; % repository directory
+else;       dir_bids = '/media/bjg335/rds-share-2018-hanslmas-memory/projects/reinstatement_fidelity';
             dir_tool = '/media/bjg335/rds-share-2018-hanslmas-memory/projects/general';
 end
 
 % add subfunctions
-addpath([dir_root,'scripts/subfunctions'])
+addpath([dir_bids,'scripts/subfunctions'])
 
 % define number of subjects
 n_subj = 21;
 
-%% Create AAL Source Grids
-% load template grid
-load([dir_tool,'fieldtrip-20170319/template/sourcemodel/standard_sourcemodel3d4mm.mat']); 
-template_grid = sourcemodel; clear sourcemodel % rename grid
-
-% load AAL atlas
-mri = ft_read_mri([dir_tool,'general/fieldtrip-20170319/template/atlas/aal/ROI_MNI_V4.nii']);
- 
-% change MRI to binary 'in-atlas' vs. 'out-atlas'
-mri.anatomy = double(mri.anatomy > 0); 
-
-% interpolate mri with grid
-cfg                 = [];
-cfg.parameter       = 'anatomy';
-cfg.interpmethod    = 'nearest';
-roi                 = ft_sourceinterpolate(cfg,mri,template_grid);
-
-% clean up
-clear mri template_grid cfg
-
-%% Decompose Source Signal
-% cycle through each subject
-for subj = 1 : n_subj
-        
-    % load data
-    load([dir_root,'data/eeg/preprocessing/subj',sprintf('%02.0f',subj),'_data.mat'])
-        
-    % load electrode layout, grid and boundary element model    
-    load([dir_root,'data/eeg/headmodels/subj',sprintf('%02.0f',subj),'/elec.mat'])
-    load([dir_root,'data/eeg/headmodels/subj',sprintf('%02.0f',subj),'/vol.mat'])
-    load([dir_root,'data/eeg/headmodels/subj',sprintf('%02.0f',subj),'/leadfield.mat'])
-    
-    % restrict grid to AAL atlas
-    grid.inside = roi.anatomy(:) > 0;
-    
-    % downsample data
-    cfg             = [];
-    cfg.resamplefs  = 50; % save some space (limits later analyses to 25Hz)
-    data            = ft_resampledata(cfg, data);
-
-    % timelock data
-    cfg                     = [];
-    cfg.covariancewindow    = 'all';
-    cfg.covariance          = 'yes';
-    cfg.keeptrials          = 'yes';
-    tml                     = ft_timelockanalysis(cfg, data);
-
-    % get filters for video and audio conditions
-    cfg                 = [];
-    cfg.method          = 'lcmv';
-    cfg.lcmv.keepfilter = 'yes';
-    cfg.lcmv.fixedori   = 'yes';
-    cfg.lcmv.lambda     = '5%';
-    cfg.headmodel       = vol;
-    cfg.elec            = elec;
-    cfg.grid            = grid;
-    tmp                 = ft_sourceanalysis(cfg, tml);
-    filter              = cell2mat(tmp.avg.filter(tmp.inside));
-    
-    % store brain geometry
-    brainGeo.dim        = tmp.dim;
-    brainGeo.inside     = tmp.inside;
-    brainGeo.pos        = tmp.pos;
-
-    % combine filters with raw video data to "beam" scalp video data to source space
-    source            = [];
-    source.time       = data.time;       % transfer time information
-    source.trialinfo  = data.trialinfo;  % transfer trial information    
-    for c = 1 : sum(grid.inside); source.label{c,1} = ['S' num2str(c)]; end % create labels for each virtual electrode    
-    for j = 1 : numel(data.trial); source.trial{1,j} = single(filter*data.trial{1,j}); end % for each trial, apply filters to the recorded data
-    
-    % save
-    save([dir_root,'data/eeg/source/subj',sprintf('%02.0f',subj),'_source.mat'],'source','brainGeo','-v7.3')
-    clear cfg aal data tml tmp filter template_grid elec grid vol brainGeo source c j
-    
-end
-
 %% Get TF of Source Data
-% predefine group data matrix
-group_freq = cell(n_subj,2);
-
-% load template grid
-load('Y:/projects/general/fieldtrip-20170319/template/sourcemodel/standard_sourcemodel3d4mm.mat'); 
-template_grid = sourcemodel; clear sourcemodel
-
 % cycle through each subject
 for subj = 1 : n_subj
+    
+    % define subject handle
+    subj_handle = sprintf('sub-%02.0f',subj);
+    dir_subj = [dir_bids,'sourcedata/',subj_handle,'/eeg/'];
     
     % load
-    load([dir_root,'data/eeg/source/subj',sprintf('%02.0f',subj),'_source.mat'])
+    load([dir_bids,'sourcedata/',subj_handle,'/eeg/',subj_handle,'_task-rf_eeg.mat'])
     
-    % find audio/video hit/miss trials
-    [audio_bool,hit_bool]   = get_splits_eeg(source);
-
+    % drop source to single precision
+    source.trial = cellfun(@single,source.trial,'UniformOutput',false);
+    
     % get time-frequency representation of data
     cfg             = [];
     cfg.keeptrials  = 'yes';
     cfg.method      = 'wavelet';
     cfg.width       = 6;
     cfg.output      = 'pow';
-    cfg.toi         = -1:0.25:3;
-    cfg.foi         = 8:2:24; % 50hz sampling
+    cfg.toi         = -1:0.1:3;
+    cfg.foi         = 8:2:30; % 50hz sampling
     cfg.pad         = 'nextpow2';
     freq            = ft_freqanalysis(cfg, source); clear source
 
@@ -137,9 +57,34 @@ for subj = 1 : n_subj
     cfg.avgoverfreq = 'yes';
     cfg.latency     = [0.5 1.5];
     freq            = ft_selectdata(cfg,freq);
-    
+        
     % save    
-    save([dir_root,'data/eeg/source/subj',sprintf('%02.0f',subj),'_freq.mat'],'freq')
+    mkdir([dir_bids,'derivatives/',subj_handle,'/eeg/'])
+    save([dir_bids,'derivatives/',subj_handle,'/eeg/',subj_handle,'_task-rf_eeg-freq.mat'],'freq','brainGeo')    
+    
+    % clean workspace
+    clear subj_handle freq avg_pow std_pow
+end
+
+%% Get Group Averages
+% predefine group data matrix
+group_freq = cell(n_subj,2);
+
+% load template grid
+load('Y:/projects/general/fieldtrip-20170319/template/sourcemodel/standard_sourcemodel3d4mm.mat'); 
+template_grid = sourcemodel; clear sourcemodel
+
+% cycle through each subject
+for subj = 1 : n_subj
+    
+    % define subject handle
+    subj_handle = sprintf('sub-%02.0f',subj);
+   
+    % load data
+    load([dir_bids,'derivatives/',subj_handle,'/eeg/',subj_handle,'_task-rf_eeg-freq.mat']);
+    
+    % find audio/video hit/miss trials
+    [audio_bool,hit_bool]   = get_splits_eeg(freq);
     
     % split video data
     cfg                     = [];
@@ -175,10 +120,6 @@ for subj = 1 : n_subj
         x(group_freq{subj,condition}.inside==1) = group_freq{subj,condition}.pow;
         group_freq{subj,condition}.pow          = x;        
     end
-    
-    % save
-    save([dir_root,'data/eeg/source/subj',sprintf('%02.0f',subj),'_freq_conditions.mat'],'freq')
-    clear cfg freq freqtmp x brainGeo audio_bool hit_bool condition
 end
    
 % get grand average
@@ -190,12 +131,13 @@ grand_hits.audio    = ft_sourcegrandaverage(cfg, group_freq{:,3});
 grand_misses.audio  = ft_sourcegrandaverage(cfg, group_freq{:,4});
 
 % save
-save([dir_root,'data/eeg/source/grand_freq.mat'],'grand_hits','grand_misses')
+mkdir([dir_bids,'derivatives/group/eeg/'])
+save([dir_bids,'derivatives/group/eeg/group_task-rf_eeg-freq.mat'],'grand_hits','grand_misses')
 keep dir_root
 
 %% Run Statistical Analysis across Whole Brain
 % load data
-load([dir_root,'data/eeg/source/grand_freq.mat'])
+load([dir_bids,'data/eeg/source/grand_freq.mat'])
 
 % get modality names
 modality = fieldnames(grand_hits);
@@ -234,11 +176,11 @@ end
 results_table = table(modality,p,ci,t,dz) %#ok<NOPTS>
 
 % save
-save([dir_root,'data/eeg/source/stat.mat'],'stat')
+save([dir_bids,'data/eeg/source/stat.mat'],'stat')
 
 %% Create Masked Source and Export
 % load data
-load([dir_root,'data/eeg/source/stat.mat'])
+load([dir_bids,'data/eeg/source/stat.mat'])
 
 % load template MRI
 mri = ft_read_mri('Y:/projects/general/fieldtrip-20170319/template/anatomy/single_subj_T1_1mm.nii');
@@ -276,7 +218,7 @@ for i = 1 : numel(modality)
     % export
     cfg = [];
     cfg.parameter     = 'pow';               % specify the functional parameter to write to the .nii file
-    cfg.filename      = [dir_root,'data/eeg/source/stat_',modality{i},'.nii'];  % enter the desired file name
+    cfg.filename      = [dir_bids,'data/eeg/source/stat_',modality{i},'.nii'];  % enter the desired file name
     cfg.filetype      = 'nifti';
     cfg.coordsys      = 'spm';
     cfg.vmpversion    = 2;
@@ -284,12 +226,12 @@ for i = 1 : numel(modality)
     ft_volumewrite(cfg,source);      % be sure to use your interpolated source data
     
     % reslice to 1mm isometric to match template MRI
-    reslice_nii([dir_root,'data/eeg/source/stat_',modality{i},'.nii'],[dir_root,'data/eeg/source/stat_',modality{i},'_sm.nii'],[1 1 1]);
+    reslice_nii([dir_bids,'data/eeg/source/stat_',modality{i},'.nii'],[dir_bids,'data/eeg/source/stat_',modality{i},'_sm.nii'],[1 1 1]);
 end
 
 %% Extract Raw Power of Cluster 
 % load data
-load([dir_root,'data/eeg/source/grand_freq.mat'])
+load([dir_bids,'data/eeg/source/grand_freq.mat'])
 
 % get indices of clusters
 clus_idx = stat.video.negclusterslabelmat==1;
@@ -307,7 +249,7 @@ writetable(tbl,[dir_repos,'data/sourceEEG.csv'],'Delimiter',',')
 
 %% Get Time-Series of Cluster
 % load cluster data
-load([dir_root,'data/eeg/source/stat.mat'])
+load([dir_bids,'data/eeg/source/stat.mat'])
 
 % predefine group data matrix
 group_freq = cell(n_subj,2);
@@ -320,7 +262,7 @@ template_grid = sourcemodel; clear sourcemodel
 for subj = 1 : n_subj
     
     % load
-    load([dir_root,'data/eeg/source/subj',sprintf('%02.0f',subj),'_source.mat'])
+    load([dir_bids,'data/eeg/source/subj',sprintf('%02.0f',subj),'_source.mat'])
     
     % find audio/video hit/miss trials
     [audio_bool,hit_bool]   = get_splits_eeg(source);
