647bc7bb03a1b777747591c62441d081dd70934c
Ben Griffiths <benjaminGriffiths@users.noreply.github.com>
2019-01-22 15:34:52 +0000

    added EEG analysis subfunctions

---
+++ subfunctions/get_memdiff_timefrequency.m
@@ -0,0 +1,101 @@
+function [freq,ntrls] = get_memdiff_timefrequency(data,operation,modality)
+
+% define time frequency analysis parameters based on data probability
+if numel(data.label) > 200
+    fprintf('More than 200 channels detected, assuming data is in source space...\n')
+    toi = 0.5:0.1:1.5;
+    foi = 8:2:30;
+else
+    fprintf('Less than 200 channels detected, assuming data is channel level...\n')
+    toi = -1:0.05:3;
+    foi = 3:0.5:40;
+end
+
+% predefine conditional arrays to include all trials
+operation_to_include = zeros(numel(data.trial),1);
+modality_to_include  = zeros(numel(data.trial),1);
+
+% predefine arrays for memory performance
+mem_performance = zeros(numel(data.trial),1);
+
+% cycle through each trial
+for trl = 1 : numel(data.trial)
+
+    % if encoding is requested#
+    if ~isempty(operation)
+        
+        % mark trials that do match specified operation
+        operation_to_include(trl) = strcmpi(data.trialinfo{trl}.operation,operation);
+     
+    % otherwise include
+    else; operation_to_include(trl) = 1;
+    end
+    
+    % if encoding is requested
+    if ~isempty(modality)
+        
+        % mark trials that do match specified operation
+        modality_to_include(trl) = strcmpi(data.trialinfo{trl}.modality,modality);
+     
+    % otherwise include
+    else; modality_to_include(trl) = 1;
+    end
+    
+    % get memory
+    mem_performance(trl) = data.trialinfo{trl}.recalled;
+end
+
+% select data
+cfg                 = [];
+cfg.trials          = operation_to_include == 1 & modality_to_include == 1;
+data                = ft_selectdata(cfg,data);
+
+% get time-frequency
+cfg                 = [];
+cfg.keeptrials      = 'yes';
+cfg.method          = 'wavelet';
+cfg.width           = 6;
+cfg.output          = 'pow';
+cfg.toi             = toi;
+cfg.foi             = foi;
+cfg.pad             = 'nextpow2';
+freq                = ft_freqanalysis(cfg, data);
+clear data
+
+% get time-averaged mean and standard deviation of power for each channel and frequency
+raw_pow = mean(freq.powspctrm,4);
+avg_pow = mean(raw_pow,1);
+std_pow = std(raw_pow,[],1);
+
+% replicate matrices to match freq.powspctrm
+avg_pow = repmat(avg_pow,[size(freq.powspctrm,1) 1 1 size(freq.powspctrm,4)]);
+std_pow = repmat(std_pow,[size(freq.powspctrm,1) 1 1 size(freq.powspctrm,4)]);
+
+% z-transform power
+freq.powspctrm = (freq.powspctrm - avg_pow) ./ std_pow;
+clear raw_pow avg_pow std_pow
+
+% split data by memory performance
+cfg         = [];
+cfg.trials  = find(mem_performance(operation_to_include == 1 & modality_to_include == 1) == 1);
+mem1        = ft_selectdata(cfg,freq);
+
+cfg         = [];
+cfg.trials  = find(mem_performance(operation_to_include == 1 & modality_to_include == 1) == 0);
+mem2        = ft_selectdata(cfg,freq);
+clear freq
+
+% get condition means
+mem1        = ft_freqdescriptives([],mem1);
+mem2        = ft_freqdescriptives([],mem2);
+
+% get difference between means
+cfg             = [];
+cfg.parameter   = 'powspctrm';
+cfg.operation   = 'subtract';
+freq            = ft_math(cfg,mem1,mem2);
+clear mem1 mem2
+
+% count number of trials in each condition
+ntrls = [sum(mem_performance(operation_to_include == 1 & modality_to_include == 1) == 1),...
+         sum(mem_performance(operation_to_include == 1 & modality_to_include == 1) == 0)];
---
+++ subfunctions/run_oneSampleT.m
@@ -0,0 +1,82 @@
+function [stat,tbl] = run_oneSampleT(cfg,data)
+
+% set default parameters if not input
+if ~isfield(cfg,'frequency'); cfg.frequency = [8 30]; end
+if ~isfield(cfg,'latency'); cfg.latency = [0.5 1.5]; end
+if ~isfield(cfg,'parameter'); cfg.parameter = 'powspctrm'; end
+if ~isfield(cfg,'tail'); cfg.tail = 0; cfg.clustertail = 0; end
+
+% if data is struct, convert to cell
+if isstruct(data); data = {data}; end
+
+% get number of data structures to analyse
+n_data = numel(data);
+
+% predefine cell for statistics
+stat = cell(n_data,1);
+
+% prepare table for stat values
+tbl = array2table(zeros(n_data,4),'VariableNames',{'t','p','ci','d'});
+
+% define statisitics
+config                   = [];
+config.uvar              = 1;
+config.ivar              = 2;
+config.method            = 'montecarlo';
+config.statistic         = 'ft_statfun_depsamplesT';
+config.correctm          = 'cluster';
+config.clusteralpha      = 0.05;
+config.numrandomization  = 2000;
+config.alpha             = 0.05;
+config.tail              = cfg.tail;
+config.clustertail       = cfg.tail;
+config.parameter         = cfg.parameter;
+
+% cycle through data structures
+for condition = 1 : n_data
+
+    % define statistics type
+    if ~isfield(data{condition},'label')
+        fprintf('No label information detected, assuming data is in source space...\n')
+        issource = true;
+    else
+        fprintf('Assuming data is channel level...\n')
+        issource = false;
+    end
+    
+    % define statistical design
+    n_subj      = size(data{condition}.(config.parameter),1);
+    config.design  = [1:n_subj, 1:n_subj; ones(1,n_subj), ones(1,n_subj)+1];
+       
+    % create null hypothesis
+    null_freq = create_null_hypothesis(data{condition},config.parameter);
+    
+    % if data is in source space
+    if issource
+        
+        % run statistics
+        cfg.dim         = data{condition}.dim;  % specify dimensions of your source grid
+        stat{condition} = ft_sourcestatistics(config, data{condition}, null_freq);
+    
+    else
+        
+        % prepare neighbours  
+        config             = [];
+        config.layout      = lay;
+        config.method      = 'triangulation';
+        config.neighbours  = ft_prepare_neighbours(config);
+
+        % run statistics
+        config.frequency   = cfg.frequency;
+        config.latency     = cfg.latency;
+        stat{condition}     = ft_freqstatistics(config, data{condition}, null_freq);
+    end
+        
+    % extract key values
+    tbl.p(condition,1)  = round(stat{condition,1}.negclusters(1).prob,3);
+    tbl.t(condition,1)  = round(stat{condition,1}.negclusters(1).clusterstat ./ sum(stat{condition,1}.negclusterslabelmat(:) == 1),3);
+    tbl.ci(condition,1) = round(stat{condition,1}.negclusters(1).cirange,3);
+
+    % calculate cohen's dz
+    tbl.d(condition,1) = round(tbl.t(condition,1)./ sqrt(n_subj),3);
+end
