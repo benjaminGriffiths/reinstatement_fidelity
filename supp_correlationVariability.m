978b0c1b9ed02fd9f69d4e81ceb4e1d3c046ccf6
Benjamin Griffiths <benjamin.griffiths.psy@gmail.com>
2019-10-07 20:37:00 +0100

    added simulation

---
+++ supp_correlationVariability.m
@@ -0,0 +1,22 @@
+noise = 0 : 0.01 : 1;
+ssivar = 0 : 0.01 : 1;
+ntrls = 100;
+r = nan(numel(noise),numel(ssivar));
+
+for i = 1 : numel(noise)
+    for j = 1 : numel(ssivar)
+        
+        % set ssi as ones plus variance
+        ssi = ones(ntrls,1) + (rand(ntrls,1)*ssivar(j));
+        
+        % set eeg as ssi plus noise
+        eeg = ssi + (rand(ntrls,1)*noise(i));
+        
+        % correlate
+        r(i,j) = corr(ssi,eeg);
+    end
+end
+
+% plot
+imagesc(ssivar,noise,r); axis xy
+xlabel('SSI Variability'); ylabel('EEG Noise')
